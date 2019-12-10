d332d2edb7a9934dc59986cd3037143241194914
jtheyer <heyerjt01@mounties.mansfield.edu>
2019-12-10 14:30:22 -0500

    -Add Icons -Ready for v2

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\DevOps\myFlutter\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\DevOps\Android\myStudioProjects\equip_inspection"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "FLUTTER_FRAMEWORK_DIR=C:\DevOps\myFlutter\flutter\bin\cache\artifacts\engine\ios"
+export "FLUTTER_BUILD_NAME=0.0.1"
+export "FLUTTER_BUILD_NUMBER=1"
